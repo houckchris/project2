d5b4052c30e62148635594190dea63013baa0575
Kristen M SPECKHALS <kristenmspeckhals@Kristens-MacBook-Pro.local>
2018-04-26 14:16:53 -0400

    models updated

---
+++ models/events.js
@@ -0,0 +1,72 @@
+module.exports = function (sequelize, DataTypes) {
+  var Event = sequelize.define("Event", {
+    eventTitle: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    eventDesc: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+
+    },
+    eventDate: {
+      type: DataTypes.DATE,
+      allowNull: false,
+
+    },
+
+    eventTime: {
+      type: DataTypes.TIME,
+      allowNull: false,
+
+    },
+    eventZip : {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        len: [5]
+      }
+
+    },
+
+    eventDuration : {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+
+    eventSlots : {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+
+    eventCat: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      len: [1]
+    }
+  });
+
+  Event.associate = function (models) {
+    // We're saying that an event should belong to an Org
+    // An Event can't be created without an Org due to the foreign key constraint
+    Event.belongsTo(models.Org, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+
+  return Event;
+};
