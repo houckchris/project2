65e64934d26929da28bf257a8b80bf74a6e672b5
Kristen M SPECKHALS <kristenmspeckhals@Kristens-MacBook-Pro.local>
2018-04-28 14:17:40 -0400

    updated vol.js so it renders by id

--- public/js/vol.js
+++ public/js/vol.js
@@ -1,19 +1,22 @@
 $(document).ready(function() {
 
     
-    $.get("/api/vols", function(data) {
+    $.get("/api/vols/1", function(data) {
  
         console.log(data)
-// Render volunteer data to the html for a specific volunteer using index point in the data array
+// Render volunteer data to the html for a specific volunteer using a specific id in the get method
 //Ideally, this would be based on a login that would use the volunteers id in the table to access their data
+   
 
- 
-            $("#volImage").attr("src", data[0].volImage);
-            $('#volName').text(data[0].volName);
-            $('#aboutMe').text(data[0].aboutMe);
-            $('#volZip').text(data[0].volZip);
-            $('#volEmail').text(data[0].volEmail);
-            $('#volSkills').text(data[0].volskills);       
+
+            $("#volImage").attr("src", data.volImage);
+            $('#volName').text(data.volName);
+            $('#aboutMe').text(data.aboutMe);
+            $('#volZip').text(data.volZip);
+            $('#volEmail').text(data.volEmail);
+            $('#volSkills').text(data.volskills);    
  
 });  
+
+
 });  
